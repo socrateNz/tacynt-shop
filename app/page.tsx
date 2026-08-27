cc2368cdc450ed690e7f1f4ac35a6f6e659bc958
socrateNz <sssnzogning0@gmail.com>
2026-08-27 16:42:05 +0100

    feat: initialize home page with layout and static content

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>Ca va aller tres vite</h1>
+      <h1 className="text-2xl font-bold">Ca va aller tres vite</h1>
+      <p className="text-lg font-medium">C'est la verite</p>
     </div>
   );
 }
